f7e3b90ffcb2f4548d7343d3102f4d1c49e83bef
Samuel Newman <mozzius@protonmail.com>
2024-10-23 21:07:24 +0300

    inequality

--- src/app/purge/actions.ts
+++ src/app/purge/actions.ts
@@ -5,11 +5,11 @@ import { redirect } from "next/navigation";
 
 export async function purgeCache(formData: FormData) {
   const password = formData.get("password");
-  if (password !== process.env.PURGE_PASSWORD) {
+  if (password === process.env.PURGE_PASSWORD) {
     revalidatePath("/", "page");
     revalidatePath("/post/[rkey]", "page");
   } else {
-    throw new Error("Invalid password, got: " + password);
+    throw new Error("Invalid password");
   }
   redirect('/')
 }
