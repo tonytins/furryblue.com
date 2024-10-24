dc43553a23b6e3232545d4c0213b96bac62aead4
Samuel Newman <mozzius@protonmail.com>
2024-10-24 14:44:34 +0300

    maybe types are wrong?

--- src/app/rss/route.ts
+++ src/app/rss/route.ts
@@ -8,20 +8,21 @@ export const revalidate = 3600; // 1 hour
 export async function GET() {
   const posts = await getPosts();
 
-  const rss = new RSS(
-    {
-      title: "mozzius.dev",
-      feed_url: "https://mozzius.dev/rss",
-      site_url: "https://mozzius.dev",
-      description: "a webbed site",
-    },
-    posts.map((post) => ({
+  const rss = new RSS({
+    title: "mozzius.dev",
+    feed_url: "https://mozzius.dev/rss",
+    site_url: "https://mozzius.dev",
+    description: "a webbed site",
+  });
+
+  for (const post of posts) {
+    rss.item({
       title: post.value.title ?? "Untitled",
       description: post.value.content,
       url: `https://mozzius.dev/post/${post.uri.split("/").pop()}`,
       date: new Date(post.value.createdAt ?? Date.now()),
-    })),
-  );
+    });
+  }
 
   return new Response(rss.xml(), {
     headers: {
