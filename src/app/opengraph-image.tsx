f78edbd6f83980a710e6b32ec6a8877ee9168ee6
Samuel Newman <mozzius@protonmail.com>
2024-10-25 09:50:50 +0300

    eslint config prettier

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,5 +1,3 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
 import { ImageResponse } from "next/og";
 
 import { loadGoogleFont } from "#/lib/google-font";
--- src/app/post/[rkey]/opengraph-image.tsx
+++ src/app/post/[rkey]/opengraph-image.tsx
@@ -1,5 +1,3 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
 import { ImageResponse } from "next/og";
 
 import { getPost } from "#/lib/api";
