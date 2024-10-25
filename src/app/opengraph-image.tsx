734c18973f172600dbc636d39902a449c411f358
Samuel Newman <mozzius@protonmail.com>
2024-10-25 09:17:10 +0300

    Update opengraph-image.tsx

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -2,6 +2,7 @@ import { readFile } from "node:fs/promises";
 import { join } from "node:path";
 import { ImageResponse } from "next/og";
 
+export const dynamic = 'force-dynamic';
 export const size = {
   width: 1200,
   height: 630,
