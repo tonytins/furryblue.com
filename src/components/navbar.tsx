9e11e3962a2f0d7161c91328d2902d5c8458a3df
Tony Bark <mnctnh817@mozmail.com>
2024-11-02 01:55:37 -0400

    Fixed path error in nav bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ export function NavBar() {
     return (
         <div className="navbar">
             <Link href="/"><HouseIcon className="text-inherit inline size-3.5 mb-0.5" /> Home</Link>
-            <Link href="links"><GlobeIcon className="text-inherit inline size-3.5 mb-0.5" /> Links</Link>
+            <Link href="/links"><GlobeIcon className="text-inherit inline size-3.5 mb-0.5" /> Links</Link>
             <Link href="about"><CircleHelpIcon className="text-inherit inline size-3.5 mb-0.5" /> About</Link>
         </div>
     );
