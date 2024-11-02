0e6576cf4afc9a22915454e183d0dd54cb188afa
Tony Bark <mnctnh817@mozmail.com>
2024-11-01 22:13:18 -0400

    Nav bar icons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import { HouseIcon, GlobeIcon, CircleHelpIcon } from "lucide-react";
 
 export function NavBar() {
     return (
         <div className="navbar">
-            <Link href="/">Home</Link>
-            <Link href="links">Links</Link>
-            <Link href="#">About</Link>
+            <Link href="/"><HouseIcon className="text-inherit inline size-3.5 mb-0.5" /> Home</Link>
+            <Link href="links"><GlobeIcon className="text-inherit inline size-3.5 mb-0.5" /> Links</Link>
+            <Link href="about"><CircleHelpIcon className="text-inherit inline size-3.5 mb-0.5" /> About</Link>
         </div>
     );
 }
