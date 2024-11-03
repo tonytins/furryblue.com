486e3d385075701da79ab49050d6555ddb2f35bd
Tony Bark <mnctnh817@mozmail.com>
2024-11-03 11:28:59 -0500

    Switched Info link to Apps in menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
-import { HouseIcon, GlobeIcon, CircleHelpIcon } from "lucide-react";
+import { HouseIcon, AppWindowIcon, GlobeIcon } from "lucide-react";
 
 export function NavBar() {
     return (
         <div className="navbar">
             <Link href="/"><HouseIcon className="text-inherit inline size-3.5 mb-0.5" /> Home</Link>
             <Link href="/links"><GlobeIcon className="text-inherit inline size-3.5 mb-0.5" /> Links</Link>
-            <Link href="about"><CircleHelpIcon className="text-inherit inline size-3.5 mb-0.5" /> Info</Link>
+            <Link href="/apps"><AppWindowIcon className="text-inherit inline size-3.5 mb-0.5" /> Apps</Link>
         </div>
     );
 }
